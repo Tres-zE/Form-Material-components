f4fb4e83f072be0f18770f458f5fdebaff2a2f03
Migue-San <lx15726@innovaccion.mx>
2023-05-22 16:12:35 -0600

    USANDO HOOKS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import FormSingUp from './Components/FormSingUp';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 
 function App() {
+
+  const handleSubmit = (valores) => {
+    console.log("AppJs: ",valores)
+  }
+
   return (
-    <>
-      <h1>Formulario Registro</h1>
-      <FormSingUp/>
-    </>
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3'align='center' component="h1">Formulario Registro</Typography>
+      <FormSingUp handleSubmit = {handleSubmit}/>
+    </Container>
   );
 }
 
--- src/Components/FormSingUp.jsx
+++ src/Components/FormSingUp.jsx
@@ -1,19 +1,89 @@
+import { useState } from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 
-function FormSingUp() {
+function FormSingUp({handleSubmit}) {
+ 
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [promo, setPromo] = useState(true)
+  const [novedades, setNovedades] = useState(false)
+
   return (
-    <form>
-      <label>Nombre</label>
-      <input type="text" />
-      <label>Apellido</label>
-      <input type="text" />
-      <label>Correo electronico</label>
-      <input type="email" />
-      <label>Promociones</label>
-      <input type="checkbox" />
-      <label>Novedades</label>
-      <input type="checkbox" />
-      <Button variant='contained'>Registrarse</Button>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      handleSubmit({name, lastName, email, promo, novedades})
+    }}>
+      <TextField
+        id="name"
+        label="Nombre"
+        variant='outlined'
+        fullWidth
+        margin='normal'
+        onChange={(e) => 
+          setName(e.target.value)
+        }
+        value={name}
+      />
+
+      <TextField
+        id="lastName"
+        label="Apellidos"
+        variant='outlined'
+        fullWidth
+        margin='normal'
+        onChange={(e) => 
+          setLastName(e.target.value)
+        }
+        value={lastName}
+      />
+
+      <TextField
+        id="email"
+        label="Email"
+        variant='outlined'
+        fullWidth
+        margin='normal'
+        onChange={(e) => 
+          setEmail(e.target.value)
+        }
+        value={email}
+      />
+
+      <FormGroup>
+        <FormControlLabel
+           control={
+              <Switch 
+                checked = {promo}
+                onChange={(e) => 
+                  setPromo(e.target.checked)
+                }
+              />
+            } 
+           label="Promociones"
+          />
+          
+          <FormControlLabel
+           control={
+           <Switch 
+            checked = {novedades}
+            onChange={(e) => 
+              setNovedades(e.target.checked)
+            }
+           />
+          } 
+           label="Novedades"
+        />
+          
+      </FormGroup>
+      
+      <Button variant='contained' type='submit'>Registrarse</Button>
+      
     </form>
   )
 }
